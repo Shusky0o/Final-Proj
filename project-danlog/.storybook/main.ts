66a9963635897f4608e7e90b0b393deee8244068
Shusky0o <neilahman.garcesa-24@cpu.edu.ph>
2026-04-27 01:40:51 +0800

    chore: impelemnt story for new order

--- project-danlog/.storybook/main.ts
+++ project-danlog/.storybook/main.ts
@@ -1,19 +1,25 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
+    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs"
   ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "..\\public"
-  ]
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
+  },
+  staticDirs: ["../public"],
+  // ensure Vite processes PostCSS/Tailwind
+  viteFinal: async (config) => {
+    return config;
+  },
 };
 export default config;
